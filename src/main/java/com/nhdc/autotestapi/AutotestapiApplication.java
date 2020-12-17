1524ffb4c9acd25f418d7943aa9cc56117dfe5a0
allenvan13 <allenvan13@163.com>
2020-12-17 19:10:00 +0800

    first commit

---
+++ src/main/java/com/nhdc/autotestapi/AutotestapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhdc.autotestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhdc/autotestapi/AutotestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhdc.autotestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
